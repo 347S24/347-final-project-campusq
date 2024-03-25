dcf7ebe0927e83e67798c060fd3c0fa00c9a019c
Wynn Ames <wynnames123@gmail.com>
2024-03-25 15:56:39 -0400

    added scaffolding to frontend

--- campusq/frontend/src/main.jsx
+++ campusq/frontend/src/main.jsx
@@ -3,22 +3,32 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Login from "./components/login/Login.jsx";
 import Student from "./components/student/Student.jsx";
+import Instructor from "./components/instructor/Instructor.jsx";
+import EditQuestions from "./components/instructor/editquestions/EditQuestions.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/student',
+    path: "/student",
     element: <Student />,
-  }
+  },
+  {
+    path: "/instructor",
+    element: <Instructor />,
+  },
+  {
+    path: "/instructor/edit",
+    element: <EditQuestions />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
