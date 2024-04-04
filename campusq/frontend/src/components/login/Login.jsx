424213a6f67618a8b081443dcc1224b6906e2281
Wynn Ames <wynnames123@gmail.com>
2024-04-04 11:10:38 -0400

    blah

--- campusq/frontend/src/components/login/Login.jsx
+++ campusq/frontend/src/components/login/Login.jsx
@@ -3,13 +3,13 @@ import "./Login.css";
 export default function Login() {
   const studentRedirect = () => {
     window.location.href =
-      "https://canvas.jmu.edu/login/oauth2/auth?response_type=code&client_id=190000000000938&redirect_uri=http://localhost:8000/accounts/canvas/login/callback/&scope=url:GET|/api/v1/users/:user_id/profile";
+      "https://canvas.jmu.edu/login/oauth2/auth?response_type=code&client_id=190000000000938&redirect_uri=http://127.0.0.1:8000/accounts/canvas/login/callback/&scope=url:GET|/api/v1/users/:user_id/profile";
   };
 
-    return (
+  return (
     <div id="main">
       <h1 id="title">CampusQ</h1>
-      <div id='buttons'>
+      <div id="buttons">
         <button className="login-button">Teacher Login</button>
         <button onClick={studentRedirect} className="login-button">
           Student Login
