48d401a1734267d57bd5b92625b26faf8194fd40
Thomas Brickhouse <thomas@halebob.net>
2024-04-11 09:48:42 -0400

    Student Code Page work

--- campusq/frontend/src/components/login/Login.jsx
+++ campusq/frontend/src/components/login/Login.jsx
@@ -3,8 +3,9 @@ import "./Login.css";
 export default function Login() {
   const studentRedirect = () => {
     window.location.href =
-      "https://canvas.jmu.edu/login/oauth2/auth?response_type=code&client_id=190000000000938&redirect_uri=http://localhost:8000/accounts/canvas/login/callback/&scope=url:GET|/api/v1/users/:user_id/profile";
-  };
+      // "https://canvas.jmu.edu/login/oauth2/auth?response_type=code&client_id=190000000000938&redirect_uri=http://localhost:8000/accounts/canvas/login/callback/&scope=url:GET|/api/v1/users/:user_id/profile";
+      "student/code"
+    };
 
     return (
     <div id="main">
